48109e3a972c1ab3ca90e7abb5ba2536dfe540c4
Shay Wagner <srwagner916@gmail.com>
2021-04-08 19:22:35 -0500

    added comments

--- assets/js/cookIt.js
+++ assets/js/cookIt.js
@@ -2,9 +2,21 @@ ingredientsArr = [];
 
 //get value from ingredientName
 $('#ingredientInput').submit(function(event){
+  // set value of the ingredient input to a variable
   var ingredient = $('#ingredientName').val();
+  // append that ingredient to the list on the page
   $('#ingredientList').append('<li>' + ingredient + '</li>');
+  // push the ingredient to an array
   ingredientsArr.push(ingredient);
+  // clear the ingredient input after a submit
   $('#ingredientName').val('');
   event.preventDefault();
-})
+});
+
+fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=chicken&320262843fmsh4a1ae8efe85b19ap1f26d7jsn8212be03ceae")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data) {
+      console.log(data);
+  })
