0a985fb7adfe33731b8de570464836bbde28d0bd
Shay Wagner <srwagner916@gmail.com>
2021-04-10 16:47:56 -0500

    map is added to page on zipcode submit

--- assets/js/cookIt.js
+++ assets/js/cookIt.js
@@ -13,6 +13,7 @@ $('#ingredientInput').submit(function(event){
   ingredientsArr.push(ingredient);
   // clear the ingredient input after a submit
   $('#ingredientName').val('');
+  spoonFetch();
   event.preventDefault();
 });
 
@@ -23,3 +24,36 @@ fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=chicken&320262843fms
   .then(function(data) {
       console.log(data);
   })
+
+var spoonFetch = function(){ fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredientsArr[0] + ",+" + ingredientsArr[1] + ",+" + ingredientsArr[2] + ",+" + ingredientsArr[3] + ",+" + ingredientsArr[4] + ",+" + ingredientsArr[5] + ",+" + ingredientsArr[6] + ",+" + ingredientsArr[7] + ",+" + ingredientsArr[8] + "&apiKey=5f1feb82b9db4dad987ffd0fc801c43b")
+  .then(function(response){
+     return response.json();
+   })
+   .then(function(data){
+   console.log(data);
+   })
+};
+
+
+// function to generate a map
+var generateMap = function(){
+  // get the latitude and longitude coordinates used to center map on location
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // assign lat and long values to variables
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    // assign the mapContainer div element to a variable
+    var mapContainerEl = $('#mapContainer');
+    // assign zipcode value to a variable
+    zipCode = $('#zipCodeInput').val();
+    // call the google embed maps api to create an iframe user the query grocery+near+zipcode, center on lat and long coordinates, and zoom level 10
+    mapContainerEl.html(`<iframe width='450' height='250' frameborder='0' style='border:0' src= 'https://www.google.com/maps/embed/v1/search?key=AIzaSyBvqgE3J3k27e7smiiZAgKLa0bsGoNuHys&q=grocery+near+${zipCode}&center=${lat},${long}&zoom=10' allowfullscreen> </iframe>`)
+  })
+};
+// event listener to generate map on zipcode submit
+$('#zipCodeForm').submit(function(event){
+  generateMap();
+  event.preventDefault();
+});
+
+
