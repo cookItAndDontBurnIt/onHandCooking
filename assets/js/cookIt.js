5883dd8f9c0e8dc318eef6e8b5aa52f73f03d554
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-04-10 18:54:42 -0500

    created the function and then fetched

--- assets/js/cookIt.js
+++ assets/js/cookIt.js
@@ -1,35 +1,39 @@
-$("#saveButton").on("click", function () {
-  alert("it clicked");
-});
-ingredientsArr = [];
+const ingredientsArr = ["apple", "corn", "feta cheese"];
+const apiKey = "5f1feb82b9db4dad987ffd0fc801c43b";
+const baseUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=`;
+
 
 //get value from ingredientName
-$('#ingredientInput').submit(function(event){
-  // set value of the ingredient input to a variable
-  var ingredient = $('#ingredientName').val();
-  // append that ingredient to the list on the page
-  $('#ingredientList').append('<li>' + ingredient + '</li>');
-  // push the ingredient to an array
-  ingredientsArr.push(ingredient);
-  // clear the ingredient input after a submit
-  $('#ingredientName').val('');
-  spoonFetch();
-  event.preventDefault();
+$('#ingredientSubmit').click(function (event) {
+    // set value of the ingredient input to a variable
+    const ingredient = $('#ingredientName').val();
+    // append that ingredient to the list on the page
+    $('#ingredientList').append('<li>' + ingredient + '</li>');
+    // push the ingredient to an array
+    ingredientsArr.push(ingredient);
+    // clear the ingredient input after a submit
+    $('#ingredientName').val('');
+    console.log(`Adding ingredient ${ingredient}`);
+    fetchRecipes();
+    event.preventDefault();
 });
 
-fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=chicken&320262843fmsh4a1ae8efe85b19ap1f26d7jsn8212be03ceae")
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data) {
-      console.log(data);
-  })
-
-var spoonFetch = function(){ fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredientsArr[0] + ",+" + ingredientsArr[1] + ",+" + ingredientsArr[2] + ",+" + ingredientsArr[3] + ",+" + ingredientsArr[4] + ",+" + ingredientsArr[5] + ",+" + ingredientsArr[6] + ",+" + ingredientsArr[7] + ",+" + ingredientsArr[8] + "&apiKey=5f1feb82b9db4dad987ffd0fc801c43b")
-  .then(function(response){
-     return response.json();
-   })
-   .then(function(data){
-   console.log(data);
-   })
-};
+function fetchRecipes() {
+    let finalURL = baseUrl;
+    for (let i = 0; i < ingredientsArr.length; i++) {
+        if (i == ingredientsArr.length - 1) {
+            finalURL = `${finalURL}${ingredientsArr[i]}`;
+        } else {
+            finalURL = `${finalURL}${ingredientsArr[i]},+`;
+        }
+    }
+    fetch(finalURL)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            //data.image, data.title,
+            // data.
+            
+            
+        });
+}
